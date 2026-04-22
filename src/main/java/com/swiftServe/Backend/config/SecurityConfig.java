c42e80ae6414bc7f15c70ee19ba93a2e237de5fe
shlok <raskarshlok07@gmail.com>
2026-04-22 12:24:57 +0530

    Implemented the JwtAuthentication Filter Module

--- src/main/java/com/swiftServe/Backend/config/SecurityConfig.java
+++ src/main/java/com/swiftServe/Backend/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.swiftServe.Backend.config;
 
+import com.swiftServe.Backend.repository.UserRepo;
+import com.swiftServe.Backend.security.JwtAuthenticationFilter;
+import com.swiftServe.Backend.security.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,28 +10,35 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth-> auth.requestMatchers(HttpMethod.POST,"/api/v1/users/register", "/api/v1/users/login").permitAll()
                         .anyRequest().authenticated()
         )
-                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-       return new BCryptPasswordEncoder();
-    }
+
 }
 
